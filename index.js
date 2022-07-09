66fd17e5a259069a0704b24c3383221cb8512a14
larimar1212 <larimarfondeur@gmail.com>
2022-07-09 15:23:07 -0400

    trying not working

--- index.js
+++ index.js
@@ -12,12 +12,15 @@
 const testUrl = 'https://www.reddit.com/r/all/top.json?limit=5&t=day'
 const baseUrl = 'https://www.reddit.com/r/'
 const topFilter = '/top.json?limit=5&t=day' // 10 year by def
-const header = document.getElementById('test-div')
+const div = document.getElementById('random-images')
+
 // NODES
 // let input = document.getElementById('') // input field
 // let subredditStr = input.value
 // realization: get a post, (subreddit, title) and an image (thumbnail or default)
 // fetch req
+
+let thisUrl = ''
 const fetchReddit = (sub) => {
     return fetch(sub) //url
         .then((response) => response.json())
@@ -26,9 +29,24 @@ const fetchReddit = (sub) => {
         })
         .catch(error => console.warn(error));
 }
-fetchReddit(testUrl).then(obj => console.log(obj.data.children[0].data.preview.images[0].resolutions[1].url))
+fetchReddit(testUrl).then((obj) => {
+    let me = obj.data.children[0].data.preview.images[0].resolutions[1].url
+    getImage(me)
+})
 
 
-fetchReddit(testUrl)
+thisUrl = fetchReddit(testUrl)
 
-    
+const getImage = (url) => {
+    const img = document.createElement('img')
+    img.src = url
+    div.append(img)
+}
+// GOALS:
+// create a function that will
+// add an img element to the div id="random-images" with the source w/e the fuck is on line 29
+
+// the function should take an argument fetchReddit(testUrl)
+
+
+getImage
